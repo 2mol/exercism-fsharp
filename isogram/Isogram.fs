96647cc45995c31d9e81782afad2c05f4eb82f5b
2mol <2mol@users.noreply.github.com>
2020-12-22 22:20:16 +0100

    isogram solution

---
+++ isogram/Isogram.fs
@@ -0,0 +1,16 @@
+﻿module Isogram
+
+let rec isIsogramHelper (seenChars : char Set) (remainingChars : char list) : bool =
+    match remainingChars with
+    | [] -> true
+    | c::cs ->
+        if Set.contains c seenChars then false
+        else isIsogramHelper (Set.add c seenChars) cs
+
+let isIsogram (str : string): bool =
+    let alphabet = (Set.ofList ['a'..'z'])
+    str
+    |> Seq.map (System.Char.ToLower)
+    |> Seq.filter (fun char -> Set.contains char alphabet)
+    |> Seq.toList
+    |> isIsogramHelper Set.empty
