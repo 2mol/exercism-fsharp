4c4f27f6c78f7b3af7740bf53cbf40aaaa4b46da
2mol <2mol@users.noreply.github.com>
2020-12-25 13:05:51 +0100

    bank-account solution

---
+++ bank-account/BankAccount.fs
@@ -0,0 +1,95 @@
+﻿module BankAccount
+
+type AccountBalance =
+    { Balance : decimal}
+
+type BankAccount
+    = Closed
+    | Open of AccountBalance
+
+// let mkBankAccount () : BankAccount =
+//     Closed
+
+// let openAccount account =
+//     match account with
+//     | Closed -> Open { Balance = 0.0m }
+//     | Open _ -> account
+
+// let closeAccount account =
+//     match account with
+//     | Closed -> Closed
+//     | Open _ -> Closed
+
+// let getBalance account =
+//     match account with
+//     | Closed -> None
+//     | Open balance -> Some balance
+
+// let updateBalance
+//     (change : decimal)
+//     (account: byref<BankAccount>)
+//     =
+//     match account with
+//     | Closed -> account <- Closed
+//     | Open balance -> account <- Open (balance + change)
+
+
+type Msg
+    = SetState of BankAccount
+    | GetState of AsyncReplyChannel<BankAccount>
+    | UpdateBalance of decimal
+
+    // = OpenAccount
+    // | CloseAccount
+    // | GetBalance
+    // | UpdateBalance of decimal
+
+
+// let updateState oldState msg = oldState
+
+let mailboxProcessor (inbox : MailboxProcessor<Msg>) =
+    let rec messageLoop (currentState : BankAccount) = async {
+        let! msg = inbox.Receive ()
+        match msg with
+        | SetState newState -> return! messageLoop newState
+        | GetState replyChannel ->
+            replyChannel.Reply currentState
+            return! messageLoop currentState
+        | UpdateBalance amount ->
+            match currentState with
+            | Closed -> return! messageLoop Closed
+            | Open {Balance=balance} ->
+                let newState = Open { Balance = balance + amount }
+                return! messageLoop newState
+    }
+    messageLoop
+
+let mkBankAccount () =
+    MailboxProcessor<Msg>.Start(
+        fun inbox -> mailboxProcessor inbox Closed
+    )
+
+let openAccount (account : MailboxProcessor<Msg>) =
+    Open { Balance = 0.0m}
+    |> SetState
+    |> account.Post
+    account
+
+let closeAccount (account : MailboxProcessor<Msg>) =
+    Closed
+    |> SetState
+    |> account.Post
+    account
+
+let getBalance (account : MailboxProcessor<Msg>) =
+    let acc = account.PostAndReply GetState
+    match acc with
+    | Closed -> None
+    | Open { Balance = balance } -> Some balance
+
+let updateBalance
+    (amount : decimal)
+    (account : MailboxProcessor<Msg>)
+    =
+    account.Post (UpdateBalance amount)
+    account
