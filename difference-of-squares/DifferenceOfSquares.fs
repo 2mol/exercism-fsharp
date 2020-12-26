480f5e21faa699c2213160a0e76be1eb78da1104
2mol <2mol@users.noreply.github.com>
2020-12-26 11:51:04 +0100

    difference squares solution

---
+++ difference-of-squares/DifferenceOfSquares.fs
@@ -0,0 +1,11 @@
+﻿module DifferenceOfSquares
+
+let squareOfSum (number: int): int =
+    pown (List.sum [1..number]) 2
+
+let sumOfSquares (number: int): int =
+    [1..number]
+    |> List.sumBy (fun n -> pown n 2)
+
+let differenceOfSquares (number: int): int =
+    (squareOfSum number) - (sumOfSquares number)
