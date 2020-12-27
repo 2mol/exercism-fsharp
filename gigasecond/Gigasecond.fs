ed84617474d5306f1e075bc45260ed4a93c1427f
2mol <2mol@users.noreply.github.com>
2020-12-27 14:55:20 +0100

    easier solutions

---
+++ gigasecond/Gigasecond.fs
@@ -0,0 +1,8 @@
+﻿module Gigasecond
+
+open System
+
+let gigasecond : TimeSpan = TimeSpan.FromSeconds 1e9
+
+let add (beginDate : DateTime) : DateTime =
+    beginDate.Add gigasecond
---
+++ rna-transcription/RnaTranscription.fs
@@ -0,0 +1,13 @@
+﻿module RnaTranscription
+
+let translate (c : System.Char) : System.Char =
+    match c with
+    | 'G' -> 'C'
+    | 'C' -> 'G'
+    | 'T' -> 'A'
+    | 'A' -> 'U'
+    | _ -> failwith "invalid character"
+
+let toRna (dna: string): string =
+    Seq.map (string << translate) dna
+    |> String.concat ""
