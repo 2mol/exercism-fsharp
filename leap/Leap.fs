4395c3014178b8e4997114fe3f64abebb90219d4
2mol <2mol@users.noreply.github.com>
2021-01-05 11:12:08 +0100

    maybe if I improve my leap year solution the mentors will finally love me

--- leap/Leap.fs
+++ leap/Leap.fs
@@ -1,6 +1,9 @@
 ﻿module Leap
 
-let leapYear (year: int): bool =
-    if year % 400 = 0 then true
-    elif year % 100 = 0 then false
-    else year % 4 = 0
+let isDivisibleBy (n: int) (divisor: int) : bool =
+    n % divisor = 0
+
+let leapYear (year: int) : bool =
+    let yearIsDivBy = isDivisibleBy year
+    yearIsDivBy 400 || (yearIsDivBy 4 && not <| yearIsDivBy 100)
+
