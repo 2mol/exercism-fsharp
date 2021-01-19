d9e8987eca8ab2201022600d737810b201c5b526
2mol <2mol@users.noreply.github.com>
2021-01-19 18:19:45 +0100

    leap mentor feedback

--- leap/Leap.fs
+++ leap/Leap.fs
@@ -1,9 +1,7 @@
 ﻿module Leap
 
-let isDivisibleBy (n: int) (divisor: int) : bool =
-    n % divisor = 0
-
 let leapYear (year: int) : bool =
-    let yearIsDivBy = isDivisibleBy year
-    yearIsDivBy 400 || (yearIsDivBy 4 && not <| yearIsDivBy 100)
+    let yearIsDivBy (divisor: int) =
+        year % divisor = 0
+    yearIsDivBy 400 || (yearIsDivBy 4 && not (yearIsDivBy 100))
 
